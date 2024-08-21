bf6f2e1d32ca2bd68738521dbbade224f91cc7ac
adu <foriteration@gmail.com>
2024-08-21 10:01:07 +0800

    feat: enable configuration

--- server/main.go
+++ server/main.go
@@ -5,50 +5,78 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"gopkg.in/yaml.v2"
 )
 
+type Config struct {
+	ChainID     uint64 `yaml:"chain_id"`
+	Provider    string `yaml:"provider"`
+	GRPCPort    string `yaml:"grpc_port"`
+	JSONRPCPort string `yaml:"json_rpc_port"`
+}
+
 func main() {
+	config := loadConfig("config.yaml")
+
 	// Start gRPC server
-	go func() {
-		lis, err := net.Listen("tcp", ":50051")
-		if err != nil {
-			panic(err)
-		}
-		s := grpc.NewServer()
-		proofServer := NewProofServer(17000, "")
-		// Register your gRPC service here
-		RegisterProofServiceServer(s, proofServer)
-		fmt.Println("gRPC server listening on :50051")
-		if err := s.Serve(lis); err != nil {
-			panic(err)
-		}
-	}()
+	go startGRPCServer(config)
 
 	// Start HTTP server (gRPC-Gateway)
-	go func() {
-		ctx := context.Background()
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
-
-		mux := runtime.NewServeMux()
-		opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-
-		// Register your gRPC-Gateway handler here
-		err := RegisterProofServiceHandlerFromEndpoint(ctx, mux, "localhost:50051", opts)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("gRPC-Gateway server listening on :8080")
-		if err := http.ListenAndServe(":8080", mux); err != nil {
-			panic(err)
-		}
-	}()
+	go startHTTPServer(config)
 
 	// Keep the main goroutine alive
 	select {}
+}
+
+func loadConfig(filename string) Config {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading config file: %v", err))
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing config file: %v", err))
+	}
+
+	return config
+}
+
+func startGRPCServer(config Config) {
+	lis, err := net.Listen("tcp", ":"+config.GRPCPort)
+	if err != nil {
+		panic(err)
+	}
+	s := grpc.NewServer()
+	proofServer := NewProofServer(config.ChainID, config.Provider)
+	RegisterProofServiceServer(s, proofServer)
+	fmt.Printf("gRPC server listening on :%s\n", config.GRPCPort)
+	if err := s.Serve(lis); err != nil {
+		panic(err)
+	}
+}
+
+func startHTTPServer(config Config) {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	err := RegisterProofServiceHandlerFromEndpoint(ctx, mux, "localhost:"+config.GRPCPort, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("gRPC-Gateway server listening on :%s\n", config.JSONRPCPort)
+	if err := http.ListenAndServe(":"+config.JSONRPCPort, mux); err != nil {
+		panic(err)
+	}
 }
